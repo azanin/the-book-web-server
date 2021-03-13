7950630090da2225b50fe8b31919f4b667bf028a
azanin <ale.zanin90@gmail.com>
2021-03-13 16:10:29 +0100

    add single threaded silly web server

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,33 @@
 use std::net::{TcpListener, TcpStream};
-use std::io::Read;
+use std::io::{Read, Write};
+use std::fs;
 
 fn main() {
     let tcp_listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
     for incoming_stream in tcp_listener.incoming() {
         let incoming_stream = incoming_stream.unwrap();
-
         handle_connection(incoming_stream);
-
-        println!("Connection established!");
     }
 }
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
 
+    let get = b"GET / HTTP/1.1\r\n";
+    let (status, file_name) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+    };
 
-    stream.read(&mut buffer);
-    let str = String::from_utf8_lossy(&buffer[..]);
-    println!("Request: {}", str)
+    let response = format!(
+        "{}{}",
+        status,
+        fs::read_to_string(file_name).unwrap()
+    );
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
